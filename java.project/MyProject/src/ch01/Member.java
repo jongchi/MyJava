77d3194377aa0d8cd3aa1abd1b7e07c7077b7b0a
jongchi <sesi8624@gmail.com>
2021-05-24 18:20:39 +0900

    CHAPTER2

--- java.project/MyProject/src/ch01/Member.java
+++ java.project/MyProject/src/ch01/Member.java
@@ -1,5 +1,7 @@
 package ch01;
 
+import ch02.Person;
+
 public class Member {
 	public static void main(String[] args) {
 		System.out.println("성종원");
@@ -13,22 +15,7 @@ public class Member {
 		System.out.println(c);
 		int d = calc.divide(10, 2);
 		System.out.println(d);
-		
-		String name = "성종원";
-		int age = 31;
-		double height = 178.1d;
-		boolean hasBook = true;
-		System.out.println("이름 : " +  name + " 나이 : "+ age + " 키 "+
-							height + " JAVA 책 보유 : "+ hasBook);
-		
-		Person per1 = new Person("홍길동","010-1234-5678",
-								"910721-1234567");
-		System.out.println(per1);
-		per1.perName = "성종원";
-		per1.perPhoneNo = "010-2345-6789";
-		per1.perNo = "910721-1357913";
-		
-		System.out.println(per1);
+
 
 	}
 }
---
+++ java.project/MyProject/src/ch02/Member.java
@@ -0,0 +1,37 @@
+package ch02;
+
+import ch01.Calculator;
+
+public class Member {
+	public static void main(String[] args) {
+		System.out.println("성종원");
+		
+		Calculator calc = new Calculator();
+		int a = calc.plus(5,5);
+		System.out.println(a);
+		int b = calc.subtract(10, 3);
+		System.out.println(b);
+		int c = calc.multiply(2, 5);
+		System.out.println(c);
+		int d = calc.divide(10, 2);
+		System.out.println(d);
+		
+		String name = "성종원";
+		int age = 31;
+		double height = 178.1d;
+		boolean hasBook = true;
+		System.out.println("이름 : " +  name + " 나이 : "+ age + " 키 "+
+							height + " JAVA 책 보유 : "+ hasBook);
+		
+		Person per1 = new Person("홍길동","010-1234-5678",
+								"910721-1234567");
+		System.out.println(per1);
+		per1.perName = "성종원";
+		per1.perPhoneNo = "010-2345-6789";
+		per1.perNo = "910721-1357913";
+		
+		System.out.println(per1);
+		
+
+	}
+}
