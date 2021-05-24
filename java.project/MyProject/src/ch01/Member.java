5f74ac0a6f3cb2d9cf2eb231f3002eec5464ace5
jongchi <sesi8624@gmail.com>
2021-05-24 18:04:25 +0900

    CHAPTER02
    2챕터 연습문제

--- java.project/MyProject/src/ch01/Member.java
+++ java.project/MyProject/src/ch01/Member.java
@@ -13,5 +13,22 @@ public class Member {
 		System.out.println(c);
 		int d = calc.divide(10, 2);
 		System.out.println(d);
+		
+		String name = "성종원";
+		int age = 31;
+		double height = 178.1d;
+		boolean hasBook = true;
+		System.out.println("이름 : " +  name + " 나이 : "+ age + " 키 "+
+							height + " JAVA 책 보유 : "+ hasBook);
+		
+		Person per1 = new Person("홍길동","010-1234-5678",
+								"910721-1234567");
+		System.out.println(per1);
+		per1.perName = "성종원";
+		per1.perPhoneNo = "010-2345-6789";
+		per1.perNo = "910721-1357913";
+		
+		System.out.println(per1);
+
 	}
 }
---
+++ java.project/MyProject/src/ch01/Person.java
@@ -0,0 +1,17 @@
+package ch01;
+
+public class Person {
+	String perName;			// 이름
+	String perPhoneNo;		// 전화번호
+	String perNo; 			// 주민등록번호
+	Person(String perName, String perPhoneNo, String perNo){
+		this.perName = perName;
+		this.perPhoneNo = perPhoneNo;
+		this.perNo = perNo;
+	}
+	
+	public String toString() {
+		return "이름 : " + perName +" 전화번호 : "+ perPhoneNo +" 주민등록번호 : "+ perNo;
+	}
+	
+}
