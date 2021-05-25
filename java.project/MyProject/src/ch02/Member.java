e966eed4156ccfe0b00da1c58138e89e9b93a430
jongchi <84554083+jongchi@users.noreply.github.com>
2021-05-25 11:15:19 +0900

    CHAPTER2

--- java.project/MyProject/src/ch02/Member.java
+++ java.project/MyProject/src/ch02/Member.java
@@ -1,34 +1,45 @@
 package ch02;
 
-import ch01.Calculator;
-
 public class Member {
+// ch02.Member 실제 이름은 패키지 이름을 포함.
+// Member 클래스에 main() 메소드 안에 아래 조건의 변수를 정의해봅시다.
+//①String 타입의 이름을 저장할 수 있는 변수 name을 정의해봅시다.
+//②int 타입의 나이를 저장할 수 있는 변수 age를 정의해봅시다.
+//③double 타입의 키를 저장할 수 있는 변수 height를 정의해봅시다.
+//④boolean 타입의 JAVA책의 보유 여부를 저장할 수 있는 변수 hasBook를 정의해봅시다.
+//⑤이름과 나이, 키, 책의 보유 여부를 출력해봅시다.
+
 	public static void main(String[] args) {
+		// 변수 선언
+		// 데이터의 타입 식별 이름
+		// 데이터 타입 -> 내가 다루어야 하는 데이터의 특징에 의해서 결정
+		// 기본형 타입 8가지 -> 정수형, 실수형, bool, 문자
+		
 		System.out.println("성종원");
 		
-		Calculator calc = new Calculator();
-		int a = calc.plus(5,5);
-		System.out.println(a);
-		int b = calc.subtract(10, 3);
-		System.out.println(b);
-		int c = calc.multiply(2, 5);
-		System.out.println(c);
-		int d = calc.divide(10, 2);
-		System.out.println(d);
+		String name = "성종원";				// 이름 초기화
+		int age = 31;						// 나이 초기화
+		double height = 178.1d;				// 키 초기화	
+		boolean hasBook = true;				// 책 보유 여부 초기화
+		System.out.println("이름 : " +  name );
+		System.out.println("나이 : "+ age + "살");
+		System.out.println("키 : "+ height + "cm");
+//		System.out.println("JAVA 책 보유 : "+ hasBook);
+		if(hasBook) {					
+			System.out.println("책을 보유하고 있습니다.");
+		} else {
+			System.out.println("책을 보유하고 있지 않습니다.");
+		}
+		System.out.println();				// 정보 출력
 		
-		String name = "성종원";
-		int age = 31;
-		double height = 178.1d;
-		boolean hasBook = true;
-		System.out.println("이름 : " +  name + " 나이 : "+ age + " 키 "+
-							height + " JAVA 책 보유 : "+ hasBook);
-		
+		// String + any Type -> String + String
+										
 		Person per1 = new Person("홍길동","010-1234-5678",
-								"910721-1234567");
+								"910721-1234567");	// 생성자 통한 per1 생성
 		System.out.println(per1);
-		per1.perName = "성종원";
+		per1.perName = "성종원";	
 		per1.perPhoneNo = "010-2345-6789";
-		per1.perNo = "910721-1357913";
+		per1.perNo1 = "910721-1357913";
 		
 		System.out.println(per1);
 		
--- java.project/MyProject/src/ch02/Person.java
+++ java.project/MyProject/src/ch02/Person.java
@@ -1,17 +1,39 @@
 package ch02;
 
+// ①회원이름을 저장하는 변수
+// ②회원 전화번호 ( 000-0000-0000 )를 저장하는 변수
+// ③회원 주민등록번호 ( 000000-0000000 또는 0000000000000 )를 저장하는 변수
+
 public class Person {
 	String perName;			// 이름
-	String perPhoneNo;		// 전화번호
-	String perNo; 			// 주민등록번호
+	String perPhoneNo = "000-0000-0000";		// 전화번호
+	String perNo1 = "900000-0000000"; // 주민등록번호 : 문자형
+	long perNo2	= 9000000000000l;
+	
+	Person(){}	
+	
 	Person(String perName, String perPhoneNo, String perNo){
 		this.perName = perName;
 		this.perPhoneNo = perPhoneNo;
-		this.perNo = perNo;
+		this.perNo1 = perNo;
 	}
 	
 	public String toString() {
-		return "이름 : " + perName +" 전화번호 : "+ perPhoneNo +" 주민등록번호 : "+ perNo;
+		return "이름 : " + perName +" 전화번호 : "+ perPhoneNo +" 주민등록번호 : "+ perNo1;
 	}
 	
+	void showData() {
+		System.out.println("회원 이름 : " + perName);
+		System.out.println("전화 번호 : " + perPhoneNo);
+		System.out.println("주민등록번호1 : " + perNo1);
+		System.out.println("주민등록번호2 : " + perNo2);
+	}
+	public static void main(String[] args) {
+		// Person 인스턴스 생성
+		Person person = new Person();
+		
+		System.out.println("이름 : "+person.perName);
+		person.perName = "손흥민";
+		person.showData();
+	}
 }
