bdd9ba3c32fb0bfc9a83f2437ba555586ab6cdeb
jongchi <84554083+jongchi@users.noreply.github.com>
2021-05-25 17:32:55 +0900

    CHAPTER4
    문제 풀이

--- java.project/MyProject/src/ch04/Member.java
+++ java.project/MyProject/src/ch04/Member.java
@@ -4,13 +4,13 @@ import java.util.*;
 public class Member {
 	static final int YEAR = 2021; // 2021년도 나이 계산
 	
-	static int ageCheck(int yearOfBirth) {	// 올해 나이 계산
+	int ageCheck(int yearOfBirth) {	// 올해 나이 계산
 		int age = YEAR - yearOfBirth;
 		System.out.println("당신은 " + age + "살 입니다.");
 		return age;
 	}
 
-	static void fluShot(int age) { 	// 독감 예방 접종 여부
+	void fluShot(int age) { 	// 독감 예방 접종 여부
 		boolean ageCheck = age < 15 || age >= 65;	// 15세 미만 or 65세 이상
 		if (ageCheck) {
 			System.out.println("무료예방접종이 가능합니다.");
@@ -19,7 +19,7 @@ public class Member {
 		}
 	}
 	
-	static void healthCheck(int age) { // 건강검진 대상 여부 
+	void healthCheck(int age) { // 건강검진 대상 여부 
 		if (age >= 20) {				// 20세 이상의 성인일 경우
 			if(age%2 == 0) {			// 짝수해에는 짝수년도생 검사 대상
 				System.out.println("무료 건강검진 대상자입니다."); 
@@ -37,10 +37,13 @@ public class Member {
 		Scanner scanner = new Scanner(System.in);
 			System.out.println("태어난 년도를 입력해주세요 ex)1991");
 			int yearOfBirth = scanner.nextInt();	
-			int age = ageCheck(yearOfBirth);
-			fluShot(age); 
 			
-			healthCheck(age);
+			Member member = new Member();
+			
+			int age = member.ageCheck(yearOfBirth);
+			member.fluShot(age); 
+			
+			member.healthCheck(age);
 
 	}
 }
