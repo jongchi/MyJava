57bb18037ecb67d010b3371216061a2b8f51d99e
jongchi <84554083+jongchi@users.noreply.github.com>
2021-05-29 22:51:34 +0900

    array

--- java.project/MyProject/src/arr/ArrayEx3.java
+++ java.project/MyProject/src/arr/ArrayEx3.java
@@ -13,7 +13,19 @@ public class ArrayEx3 {
 		System.out.println("arr.length:"+arr.length);
 		for(int i=0;i<arr.length;i++) {
 			System.out.println("arr ["+i+"] = "+arr[i]);
-			
+		}
+		int[] tmp = new int[arr.length*2];
+		
+		// 배열 arr에 저장한 값들을 배열 tmp에 저장한다.
+		for(int i=0;i<arr.length;i++) {
+			tmp[i] = arr[i];
+		}
+		arr = tmp; // tmp에 저장된 값을 arr에 저장한다.
+		
+		System.out.println("[ 변경 후 ]");
+		System.out.println("arr.length:"+arr.length);
+		for(int i=0;i<arr.length;i++) {
+			System.out.println("arr ["+i+"] = "+arr[i]);
 		}
 	}
 
