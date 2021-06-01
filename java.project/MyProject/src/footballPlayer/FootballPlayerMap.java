dd66cb847a31f99809da33b8fb20ce7078b561b1
jongchi <sesi8624@gmail.com>
2021-06-01 15:04:29 +0900

    FootballPlayer

--- java.project/MyProject/src/footballPlayer/FootballPlayerMap.java
+++ java.project/MyProject/src/footballPlayer/FootballPlayerMap.java
@@ -7,10 +7,10 @@ public class FootballPlayerMap {
 		HashMap<Integer, FootballPlayer> map = new HashMap();
 			map.put(7, new FootballPlayer("김미드", 7, "이랜드", 27));
 			map.put(11, new FootballPlayer("박공격", 11, "이랜드", 23));
-			map.put(13, new FootballPlayer("이수비", 7, "울산현대", 28));
+			map.put(13, new FootballPlayer("이수비", 15, "울산현대", 28));
+			
+			for(Integer key : map.keySet())
+			System.out.println(map.get(key));
 
-			System.out.println(map.get(7));
-			System.out.println(map.get(11));
-			System.out.println(map.get(13));
 	}
 }
