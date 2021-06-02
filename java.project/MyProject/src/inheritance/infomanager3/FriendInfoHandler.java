2a7b0722ac08d44d5df939cbb78e09ed37fc4c4d
jongchi <84554083+jongchi@users.noreply.github.com>
2021-06-02 09:35:48 +0900

    update

--- java.project/MyProject/src/inheritance/infomanager3/FriendInfoHandler.java
+++ java.project/MyProject/src/inheritance/infomanager3/FriendInfoHandler.java
@@ -1,4 +1,4 @@
-package inheritance.infomanager;
+package inheritance.infomanager3;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -6,15 +6,18 @@ import java.util.Scanner;
 public class FriendInfoHandler {
 	
 	// 저장 데이터(객체)를 배열로 저장하고 관리하는 기능을 가지는 클래스
+	// 2021-05-31
+	// 배열 -> 컬렉션
+	ArrayList<Friend> friends;
 	
-
-	Friend[] friends;
-	int numOfFriend;	// 배열의 입력 index 역할, 정보의 개수
+//	Friend[] friends;
+//	int numOfFriend;	// 배열의 입력 index 역할, 정보의 개수
 	
 	// 생성자
 	public FriendInfoHandler(int num) {	// 배열의 사이즈 -> 배열 인스턴스 생성
-		this.friends = new Friend[num];
-		this.numOfFriend = 0;
+		friends = new ArrayList<Friend>(num);
+		//this.friends = new Friend[num];
+		//this.numOfFriend = 0;
 	}
 	
 	// 데이터의 입력 처리
@@ -55,7 +58,12 @@ public class FriendInfoHandler {
 	
 	// Friend 클래스를 상속하는 타입의 인스턴스를 받아 배열에 저장
 	public void addFriendInfo(Friend f) {
-		friends[numOfFriend++] = f;
+		// 배열에 요소 추가
+		//friends[numOfFriend++] = f;
+		
+		// 컬렉션 : List에 요소 추가
+		friends.add(f);
+		
 		System.out.println("정보가 저장되었습니다.");
 	}
 	
@@ -63,9 +71,13 @@ public class FriendInfoHandler {
 	public void showAllData() {
 		System.out.println("친구의 모든 정보를 출력합니다.");
 		System.out.println("--------------------------");
-		if(numOfFriend>0) {
-			for(int i=0; i<numOfFriend; i++) {
-				friends[i].showData();
+		// 2021-05-31 : 수정
+		// 요소의 저장방식이 배열 -> 컬렉션 => 반복문 수정
+		
+		if(friends.size()>0) {
+			for(int i=0; i<friends.size(); i++) {
+				//friends[i].showData();
+				friends.get(i).showData();
 				System.out.println("--------------------------");
 			}
 		} else {
@@ -77,9 +89,10 @@ public class FriendInfoHandler {
 	public void showAllBasicData() {
 		System.out.println("친구의 모든 기본 정보를 출력합니다.");
 		System.out.println("--------------------------");
-		if(numOfFriend>0) {
-			for(int i=0; i<numOfFriend; i++) {
-				friends[i].showBasicInfo();
+		// 2021-05-31 수정 : List 출력 수정
+		if(!friends.isEmpty()) {
+			for(int i=0; i<friends.size(); i++) {
+				friends.get(i).showBasicInfo();
 				System.out.println("--------------------------");
 			}
 		} else {
---
+++ java.project/MyProject/src/inheritance/infomanager3/ShowData.java
@@ -0,0 +1,8 @@
+package inheritance.infomanager3;
+
+public interface ShowData {
+	// 인스턴스의 기본 정보를 출력하느 메소드로 정의하세요.
+	
+	void showBasicInfo(); // public abstract 생략
+	
+}
