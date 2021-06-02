94e193e19d9f5f1815a31db9e73e1443f097c67d
jongchi <84554083+jongchi@users.noreply.github.com>
2021-06-02 15:58:30 +0900

    io

---
+++ java.project/MyProject/src/ex/io/ObjectSerializable.java
@@ -0,0 +1,73 @@
+package ex.io;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class ObjectSerializable {
+
+	public static void main(String[] args) {
+
+		
+		try {
+			// 인스턴스를 저장	
+			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("Object.ser"));
+			
+			out.writeObject(new Circle(1, 2, 2.4));
+			out.writeObject(new Circle(3, 5, 7.9));
+			out.writeObject("Hello");
+			out.close();
+			
+			System.out.println("인스턴스 저장 완료");
+			
+			// 인스턴스를 복원
+			ObjectInputStream in = new ObjectInputStream(new FileInputStream("Object.ser"));
+			
+			// 복원 순서는 저장 순서에 맞게 복원
+			Circle c1 = (Circle)in.readObject();
+			Circle c2 = (Circle)in.readObject();
+			String str = (String)in.readObject();
+			
+			System.out.println("복원된 인스턴스의 데이터를 출력");
+			
+			c1.showData();
+			c2.showData();
+			System.out.println(str);
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
+
+class Circle implements Serializable {
+	
+	// implements Serializable : 직렬화 대상임을 의미
+	
+	int x;
+	int y;
+	double r;
+
+	public Circle(int x, int y, double r) {
+		this.x = x;
+		this.y = y;
+		this.r = r;
+	}
+	
+	public void showData() {
+		System.out.println("원점 ["+x+","+y+"]");
+		System.out.println("반지름 : "+r);
+	}
+	
+}
