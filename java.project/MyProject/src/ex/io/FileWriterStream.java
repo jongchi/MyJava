444516d4e0b5c8b8854f112c6531eb76a1dc922a
jongchi <84554083+jongchi@users.noreply.github.com>
2021-06-02 13:21:16 +0900

    io

---
+++ java.project/MyProject/src/ex/io/FileWriterStream.java
@@ -0,0 +1,36 @@
+package ex.io;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+public class FileWriterStream {
+	public static void main(String[] args) {
+		
+		// 문자 단위 저장을 위한 출력 스트림 
+		// Writer -> FileWriter : 파일에 문자를 저장
+		try {
+			Writer out = new FileWriter("hyper.txt");
+				
+			char c1 = 'A';
+			char c2	= 'B';
+			String str = "CD";
+			
+			// 쓰기
+			out.write(c1);
+			out.write(c2);
+			out.write(str);
+			out.write('E');
+			out.write("FG");
+			
+			out.close();
+			
+			System.out.println("글쓰기 완성");
+
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
