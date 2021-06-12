6474c381130d77bafb48ea4c849636b9c47009c1
jongchi <84554083+jongchi@users.noreply.github.com>
2021-06-12 22:06:52 +0900

    sql ex4

--- java.project/SQL/20210612_sql_ex4.sql
+++ java.project/SQL/20210612_sql_ex4.sql
@@ -44,7 +44,7 @@ order by e1.empno desc
 -- 40. SELF JOIN을 사용하여 지정한 사원의 이름, 부서번호, 지정한 사원과 동일한 부서에서 근무하는 사원을 출력하시오. ( SCOTT )
 select e1.ename, e1.deptno, e2.ename as "coworker"
 from emp e1, emp e2
-where e1.deptno = e2.deptno and 2.ename='SCOTT'
+where e1.deptno = e2.deptno and e2.ename='SCOTT'
 ;
 
 -- 41. SELF JOIN을 사용하여 WARD 사원보다 늦게 입사한 사원의 이름과 입사일을 출력하시오.
