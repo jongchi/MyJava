c184143e9df33991bef1356baccc8d39fcc88c14
jongchi <sesi8624@gmail.com>
2021-06-15 17:08:40 +0900

    join & sub query ex

--- java.project/SQL/20210615_join_subquery_ex.sql
+++ java.project/SQL/20210615_join_subquery_ex.sql
@@ -1,24 +1,41 @@
 -- 2021.06.15
 -- JOIN & SUB QUERY
 
--- (5) 박지성이구매한도서의출판사수
-select count(publisher)
+-- (5)  박지성이구매한
+--      도서의출판사수
+
+-- sub query 
+select count(distinct publisher)
 from book
-where bookid in (select o.bookid
+where bookid in (select distinct o.bookid
                 from orders o , customer c
                 where o.custid = c.custid and c.name = '박지성')
 ;
 
+-- join 
+select count(distinct publisher)
+from customer c, orders o, book b
+where c.custid=o.custid and o.bookid = b.bookid
+and c.name = '박지성';
+
 -- (6) 박지성이구매한도서의이름, 가격, 정가와판매가격의차이
+
+-- sub query
 select b.bookname, b.price, b.price - o.saleprice as pricegap
 from book b, orders o
 where  b.bookid = o.bookid and
-        b.bookid in (select o.bookid
-                from orders o , customer c
-                where o.custid = c.custid and c.name = '박지성')
+        o.custid in (select custid from customer where name = '박지성')
+;
+-- join 
+select b.bookname, b.price, b.price-o.saleprice as pricegap
+from orders o, customer c, book b
+where c.custid = o.custid and o.bookid=b.bookid
+and c.name = '박지성'
 ;
 
--- (7) 박지성이구매하지않은도서의이름
+-- (7) 박지성이구매하지않은 -> sub query
+-- 도서의이름 -> main query
+
 select bookname
 from book
 where bookid not in (select o.bookid
@@ -26,12 +43,21 @@ where bookid not in (select o.bookid
                 where o.custid = c.custid and c.name = '박지성')
 ;
 
+
 -- (8) 주문하지않은고객의이름(부속질의사용)
+-- sub query
 select name
-from customer c
-where not exists (select custid from orders o 
-                    where c.custid = o.custid)
+from customer 
+where custid not in (select distinct custid from orders)
 ;
+
+--outer join
+select c.name
+from orders o, customer c
+where o.custid(+) = c.custid
+and o.orderid is null
+;
+
 
 -- (9) 주문금액의총액과주문의평균금액
 select sum(saleprice) as total, avg(saleprice) as avgsaleprice
@@ -39,13 +65,22 @@ from orders
 ;
 
 -- (10) 고객의이름과고객별구매액
+-- join
 select c.name, sum(saleprice) 
 from orders o, customer c
 where o.custid = c.custid
 group by c.name
 ;
 
--- (11) 고객의이름과고객이구매한도서목록
+--sub query
+select (select c.name from customer c where o.custid = c.custid) as name, 
+        sum(saleprice)
+from orders o
+group by o.custid
+;
+
+-- (11) 고객의 이름과 고객이 구매한 도서목록
+--sub query
 select c.name, (select bookname from book b
                 where o.bookid = b.bookid) as bookname
 from  orders o, customer c
@@ -53,12 +88,18 @@ where o.custid = c.custid
 order by c.name
 ;
 
+--join
+select name, bookname 
+from orders natural join customer natural join book
+;
+
 -- (12) 도서의가격(Book 테이블)과판매가격(Orders 테이블)의차이가가장많은주문
 
 --도서가격 - 판매가격 최대값
 select max(b.price - o.saleprice)
 from orders o, book b
-where o.bookid = b.bookid;
+where o.bookid = b.bookid
+;
 
 select *
 from orders o, book b
@@ -67,7 +108,8 @@ where o.bookid = b.bookid and
                                 from orders o, book b
                                     where o.bookid = b.bookid);
 
--- (13) 도서의판매액평균보다자신의구매액평균이더높은고객의이름
+-- (13) 도서의 판매액 평균보다 
+-- 자신의 구매액 평균이 더 높은 고객의 이름
 
 select c.name
 from customer c, orders o
