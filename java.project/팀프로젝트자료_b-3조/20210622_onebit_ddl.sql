cb882c5e7e8b94aa19eb25daba878559c17b2fb3
jongchi <84554083+jongchi@users.noreply.github.com>
2021-06-22 10:08:57 +0900

    ddl

---
+++ java.project/팀프로젝트자료_b-3조/20210622_onebit_ddl.sql
@@ -0,0 +1,38 @@
+-- 20210622
+-- One bit Class 테이블 생성
+
+create table classmember (   -- 수강생 정보 저장
+    mno number(4) constraint classmember_mno_pk primary key, -- 회원번호
+    mid varchar2(20) not null,      -- ID
+    mpw varchar2(20) not null,      -- 비밀번호
+    mname varchar2(20) not null,    -- 이름
+    mdate date,                     -- 생년월일 
+    mloc varchar2(20),              -- 선호 지역
+    mpoint integer                  -- 보유 포인트
+);
+
+create table teacher (              -- 강사 정보 저장
+    tno number(4) constraint teacher_tno_pk primary key, -- 강사 번호
+    tid varchar2(20) not null,      -- ID
+    tpw varchar2(20) not null,      -- 비밀번호
+    tname varchar2(20) not null,    -- 이름
+    tpoint integer                  -- 보유 포인트
+);
+
+create table bitclass (             -- 강좌 정보 저장     
+    cno number(4) constraint bitclass_cno__pk primary key, -- 강좌 번호
+    cloc varchar2(20) not null, -- 강좌 지역
+    startdate date not null, -- 강좌 시작일
+    enddate date not null, -- 강좌 종료일
+    fee integer not null, -- 수강료
+    rate number(2), -- 평점
+    numpeople number(2) not null, -- 수강 인원
+    tno number(4) constraint bitclass_tno_fk references teacher(tno) -- 강사 번호
+);
+
+create table classorder (
+    orderno number(4) constraint classorder_on_pk primary key,
+    mno number(4) constraint classorder_mno_fk references classmember(mno),
+    cno number(4) constraint classorder_cno_fk references bitclass(cno),
+    orderdate date default sysdate
+);
